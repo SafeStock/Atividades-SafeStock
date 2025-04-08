422c7d1b8fa9fc8ab0ba2709f96b11a1b81b8832
pedro.novaes <pedro.barros@sptech.school>
2025-04-08 11:35:23 -0300

    Att dos files

--- webSite/src/pages/ContateNos.jsx
+++
@@ -1,8 +0,0 @@
-export function ContateNos(){     
-    return(
-        <section className="bg-[#ccc] w-full h-[90vh] text-[#5390e7] ">
-         <p>Contate-nos</p>
-        </section>
-     )
- 
-}
--- webSite/src/pages/PorQueSafeStock.jsx
+++
@@ -1,8 +0,0 @@
-export function PorQueSafeStock() {
-    return(
-        <section className="bg-[#ccc] w-full h-[90vh] text-[#5390e7]">
-         <p>má poq safe stock</p>
-        </section>
-     )
- 
-}
