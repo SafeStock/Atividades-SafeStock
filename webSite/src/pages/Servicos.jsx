dba0a3fc7bd53a24d168e4852e564faf356ef889
MayaraDamas <mayara.damas@sptech.school>
2025-04-08 22:54:01 -0300

    subindo começo da seção serviços

--- webSite/src/pages/Servicos.jsx
+++ webSite/src/pages/Servicos.jsx
@@ -1,8 +1,21 @@
+import ProdutosLimpeza from "../assets/ProdutosLimpeza.png";
 export function Servicos() {
 
+
+
     return (
-        <section id="Serviços" className="bg-[#fff] w-full h-[100vh] text-[#5390e7]">
-            <p>Serviços</p>
+        <section id="Serviços" className="bg-[#fff] w-full h-[100vh] text-[#5390e7] flex flex-col items-center justify-center">
+           
+            <h1 className="font-bold text-[5vh]">Serviços</h1>
+            <p className="text-[3vh]">Conheça nossos serviços</p>
+            <div className="w-[17%]">
+                <div>
+                    <div>
+                        <img src={ProdutosLimpeza} alt="" className="w-[70%] "/>
+                    </div>
+                    <h2>Gerenciamento de Estoque</h2>
+                </div>
+            </div>
         </section>
     )
 
