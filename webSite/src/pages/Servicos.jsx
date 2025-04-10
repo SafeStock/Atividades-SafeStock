be964361f2c457d827e503d24c83777e5f4cd784
MayaraDamas <mayara.damas@sptech.school>
2025-04-10 19:25:13 -0300

    Seção serviços quase finalizada

--- webSite/src/pages/Servicos.jsx
+++ webSite/src/pages/Servicos.jsx
@@ -1,3 +1,4 @@
+import dash from "../assets/dash.png";
 import ProdutosLimpeza from "../assets/ProdutosLimpeza.png";
 export function Servicos() {
 
@@ -5,15 +6,39 @@ export function Servicos() {
 
     return (
         <section id="Serviços" className="bg-[#fff] w-full h-[100vh] text-[#5390e7] flex flex-col items-center justify-center">
-           
-            <h1 className="font-bold text-[5vh]">Serviços</h1>
-            <p className="text-[3vh]">Conheça nossos serviços</p>
-            <div className="w-[17%]">
-                <div>
-                    <div>
-                        <img src={ProdutosLimpeza} alt="" className="w-[70%] "/>
-                    </div>
-                    <h2>Gerenciamento de Estoque</h2>
+            <h1 className="text-[4.5vh]">Serviços</h1>
+            <div className="text-[#2F4672] flex flex-row gap-[3vh]">
+                <div className="h-[56vh] rounded-[30px] shadow-[3px_3px_8px_rgba(0,0,0,0.3)] flex flex-col items-center gap-[4vh]">
+                    <img src={ProdutosLimpeza} alt="" className=" h-[25vh] rounded-t-[30px] border-transparent p-4" />
+                    <h1>Gerenciamento de Estoque</h1>
+                    <span className="w-[32vh] text-center text-[2vh]">Permite o cadastro, edição e remoção de produtos
+                        de limpeza de forma simples e organizada.
+                        Tenha controle total
+                        sobre os itens disponíveis.</span>
+                </div>
+                <div className="h-[56vh] rounded-[30px] shadow-[3px_3px_8px_rgba(0,0,0,0.3)] flex flex-col items-center gap-[4vh]">
+                    <img src={dash} alt="" className=" h-[25vh] rounded-t-[30px] border-transparent p-4" />
+                    <h1>Gerenciamento de Estoque</h1>
+                    <span className="w-[32vh] text-center text-[2vh]">Permite o cadastro, edição e remoção de produtos
+                        de limpeza de forma simples e organizada.
+                        Tenha controle total
+                        sobre os itens disponíveis.</span>
+                </div>
+                <div className="h-[56vh] rounded-[30px] shadow-[3px_3px_8px_rgba(0,0,0,0.3)] flex flex-col items-center gap-[4vh]">
+                    <img src={dash} alt="" className=" h-[25vh] rounded-t-[30px] border-transparent p-4" />
+                    <h1>Gerenciamento de Estoque</h1>
+                    <span className="w-[32vh] text-center text-[2vh]">Permite o cadastro, edição e remoção de produtos
+                        de limpeza de forma simples e organizada.
+                        Tenha controle total
+                        sobre os itens disponíveis.</span>
+                </div>
+                <div className="h-[56vh] rounded-[30px] shadow-[3px_3px_8px_rgba(0,0,0,0.3)] flex flex-col items-center gap-[4vh]">
+                    <img src={dash} alt="" className=" h-[25vh] rounded-t-[30px] border-transparent p-4" />
+                    <h1>Gerenciamento de Estoque</h1>
+                    <span className="w-[32vh] text-center text-[2vh]">Permite o cadastro, edição e remoção de produtos
+                        de limpeza de forma simples e organizada.
+                        Tenha controle total
+                        sobre os itens disponíveis.</span>
                 </div>
             </div>
         </section>
