169cb535255fa667b7ce0722563914d3a2ccfd67
teodorosamuel <samuel.tsouza@sptech.school>
2025-04-16 11:43:15 -0300

    Alterações realizadas

--- webSite/src/pages/PorQueSafeStock.jsx
+++ webSite/src/pages/PorQueSafeStock.jsx
@@ -4,7 +4,7 @@ export function PorQueSafeStock() {
     return(
         <section id="Por que SafeStock" className="bg-[#eee] w-full h-[110vh] text-[#5390e7] flex justify-center items-center flex-col ">
          <p className="text-[6.5vh] text-[#3A577B] absolute mb-[55vh]">Por que escolher Safe<strong>Stock</strong>?</p>
-         <p className="text-[3.5vh] text-[#3A577B] mr-[45vw] mb-[32.5vh] absolute">Com a SafeStock <b>você tem:</b></p>
+         <p className="text-[3.5vh] text-[#3A577B] mr-[63vw] mb-[32.5vh] absolute">Com a SafeStock <b>você tem:</b></p>
          <div className=" w-full h-[40vh] flex justify-center items-center flex-row mt-[20vh]">
         <ImgReference imagemUrl="/src/assets/Controle.svg" texto="Controle fácil e intuitivo dos estoques "/>
         <ImgReference imagemUrl="/src/assets/Grafico.svg" texto="Insights em relação ao consumo"/>
